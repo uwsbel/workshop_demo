8235d29b4eee5c37c8044c0edf1cf9e69d9818e1
huweibit <huweibit@gmail.com>
2023-02-07 15:46:43 -0600

    Small

--- demos/single_wheel_real_slope_mode/demo_FSI_SingleWheelTest_RealSlope_mode.cpp
+++ demos/single_wheel_real_slope_mode/demo_FSI_SingleWheelTest_RealSlope_mode.cpp
@@ -276,7 +276,7 @@ int main(int argc, char* argv[]) {
     if (argc == 4) {
         total_mass = std::stod(argv[1]);
         slope_angle = std::stod(argv[2]) / 180.0 * CH_C_PI;
-        wheel_AngVel = = std::stod(argv[3]);
+        wheel_AngVel = std::stod(argv[3]);
         out_dir = out_dir + std::to_string(std::stoi(argv[2])) + "/";
     } else if (argc != 4) {
         std::cout << "usage: ./demo_FSI_SingleWheelTest_RealSlope_mode <total_mass> <slope_angle> <wheel_angVel>" << std::endl;
