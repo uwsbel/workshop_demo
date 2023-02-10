30300f76a439624edaafa078ca02db4fdf2665ad
harry <hzhang699@wisc.edu>
2023-02-10 11:20:57 -0600

    modify demos bash script

--- demos.sh
+++ demos.sh
@@ -28,28 +28,36 @@ cd /root/sbel/outputs/ && slope_slip_wheel_realSlope_mode.py
 echo "Finished Real Slope Mode"
 
 echo "Start Running Viper and Curiosity demo"
+cd /root/sbel/demos/viper_real_slope/build/ && ./demo_ROBOT_Viper_RealSlope 73.0 0 0.8
+cd /root/sbel/demos/viper_real_slope/build/ && ./demo_ROBOT_Viper_RealSlope 73.0 5 0.8
+cd /root/sbel/demos/viper_real_slope/build/ && ./demo_ROBOT_Viper_RealSlope 73.0 10 0.8
 cd /root/sbel/demos/viper_real_slope/build/ && ./demo_ROBOT_Viper_RealSlope 73.0 15 0.8
+cd /root/sbel/demos/viper_real_slope/build/ && ./demo_ROBOT_Viper_RealSlope 73.0 20 0.8
+cd /root/sbel/demos/viper_real_slope/build/ && ./demo_ROBOT_Viper_RealSlope 73.0 25 0.8
+cd /root/sbel/demos/viper_real_slope/build/ && ./demo_ROBOT_Viper_RealSlope 73.0 30 0.8
 cd /root/sbel//demos/curiosity_uphill/build/ && ./demo_ROBOT_Curiosity_Uphill 200.0 1.0 1
 
 echo "finish running the demos"
 
 cp /root/sbel/demos/curiosity_uphill/blender_curiosity_render.py /root/sbel/outputs/FSI_Curiosity_Uphill_1/
 cp -r /root/sbel/demos/curiosity_uphill/obj_for_render/ /root/sbel/outputs/FSI_Curiosity_Uphill_1/
-for i in {1..25}
+for i in {0..24}
 do
         cd /root/sbel/outputs/FSI_Curiosity_Uphill_1/ && blender --background --python ./blender_curiosity_render.py $i
 done
 cd /root/sbel/outputs/FSI_Curiosity_Uphill_1/ && ffmpeg -r 1 -f image2 -s 1920x1080 -i %d.png -vframes 25 -vcodec libx264 -crf 1 -pix_fmt yuv420p curiosity_animation.mp4
+
 echo "finished curiosity postprocessing"
 
 
 cp /root/sbel/demos/viper_real_slope/slope_slip_full_rover.py /root/sbel/outputs/
 cd /root/sbel/outputs/ && python3 slope_slip_full_rover.py
 
-cp /root/sbel/demos/viper_real_slope/blender_viper_render.py /root/sbel/outputs/FSI_Viper_RealSlope_SlopeAngle_15/
-cp -r /root/sbel/demos/viper_real_slope/obj_for_render/ /root/sbel/outputs/FSI_Viper_RealSlope_SlopeAngle_15/
-for i in {1..25}
+cp /root/sbel/demos/viper_real_slope/blender_viper_render.py /root/sbel/outputs/FSI_Viper_RealSlope_SlopeAngle_0/
+cp -r /root/sbel/demos/viper_real_slope/obj_for_render/ /root/sbel/outputs/FSI_Viper_RealSlope_SlopeAngle_0/
+for i in {0..19}
 do
-        cd /root/sbel/outputs/FSI_Viper_RealSlope_SlopeAngle_15/ && blender --background --python ./blender_viper_render.py $i
+        cd /root/sbel/outputs/FSI_Viper_RealSlope_SlopeAngle_0/ && blender --background --python ./blender_viper_render.py $i
 done
-cd /root/sbel/outputs/FSI_Curiosity_Uphill_1/ && ffmpeg -r 1 -f image2 -s 1920x1080 -i %d.png -vframes 25 -vcodec libx264 -crf 1 -pix_fmt yuv420p viper_animation.mp4
+cd /root/sbel/outputs/FSI_Viper_RealSlope_SlopeAngle_0/ && ffmpeg -r 1 -f image2 -s 1920x1080 -i %d.png -vframes 20 -vcodec libx264 -crf 1 -pix_fmt yuv420p viper_animation.mp4
+echo "finished viper postprocessing"
